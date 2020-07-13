2e2c5e7625f7e6cc66de2934810f012cb77ac3c6
marru <marrufojosecarlos@gmail.com>
2020-07-13 02:17:23 -0500

    test

--- start/routes.js
+++ start/routes.js
@@ -16,7 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', ({ response }) => {
+Route.get('/api', ({ response }) => {
     response.send("esta es la api")
 })
 
