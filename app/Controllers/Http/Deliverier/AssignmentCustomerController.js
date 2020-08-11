c794ecccad1eff0b8af5f0e817c2be6cf71c9f31
marru <marrufojosecarlos@gmail.com>
2020-08-11 18:37:17 -0500

    Up warriors (:

--- app/Controllers/Http/Deliverier/AssignmentCustomerController.js
+++ app/Controllers/Http/Deliverier/AssignmentCustomerController.js
@@ -1,14 +1,20 @@
 'use strict'
 const Employee = use('App/Models/Employees/Employee')
+const moment = use('moment')
+
 
 class AssignmentCustomerController {
     async index ({ request, response, auth }) {
+        const today = moment().format('YYYY-MM-DD')
         const user = await auth.getUser()
         const assignCustomer =  await user.employee()
             .with('assignCustomer', builder => {
-                builder.where("is_active", true)
-                    .with('customers')
-            }).fetch() 
+                builder.whereHas('details', builder => {
+                    builder.where({ status: 0 , delivery_date: today})
+                } ,1)
+                .with('customers')
+            })
+            .fetch() 
             
         return response.ok(assignCustomer)
     }
